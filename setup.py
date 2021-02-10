282b21165dc9886f1365a9e0260397fbadb36670
Amilcar <amilcar.sampaio@gmail.com>
2021-02-10 17:01:25 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from distutils.core import setup
-
 setup(
     name='hub_dados',
-    version='1.0.0',
+    version='1.1.0',
     packages=['hub_dados'],
     url='https://github.com/amisampaio/hub_dados',
     license='Apache 2.0',
